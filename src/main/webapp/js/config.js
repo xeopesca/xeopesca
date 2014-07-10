3255a941cd70bdac5b4a325f72fda1ad5420e1e4
pbelay <pbelay@gmail.com>
2014-07-10 21:05:35 +0200

    Url correcta para acceso ao geoserver_wfs

--- src/main/webapp/js/config.js
+++ src/main/webapp/js/config.js
@@ -2,6 +2,6 @@
  * Ficheiro de configuración global URL_GEOSERVER FEATURE_NS
  */
 
-var URL_GEOSERVER = "http://localhost:8080/geoserver/wfs";
+var URL_GEOSERVER = "/geoserver/wfs";
 var FEATURE_NS = "http://localhost:8080/xeopesca";
 var MAX_FEATURE= 30;
